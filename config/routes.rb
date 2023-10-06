c04aa62339ed9b31276c3003425f0ef4f7a9b873
victor barbosa <barbosavictor30@gmail.com>
2023-10-06 12:58:35 -0300

    criando primeiras paginas .html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get "/contato", to: "pages#contato"
+  get "/sobre", to: "pages#sobre"
+  get "/solucoes", to: "pages#solucoes"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
